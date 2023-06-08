0fd6fdd44a87145ea51febeaa857d346f1eec013
Marcus Boykin <awa@Marcuss-MBP.lan>
2023-06-08 07:17:52 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
+import {format} from "date-fns"
 
 function App() {
+  const [items, setItems] = useState([])
+  const [query, setQuery] = useState("Programming")
+  const [text, setText] = useState("")
+  const [largeTitle, setLargeTitle] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setIsLoading(true)
+      const fetchArticles = async() => {
+        const res = await fetch (`https://hn.algolia.com/api/v1/search?query=${query}`)
+        const data = await res.json()
+        setItems(data.hits)
+        setLargeTitle(data.hits[0])
+      }
+
+      fetchArticles()
+      setIsLoading(false)
+  }, [query])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!text){
+      // do something
+    } else {
+      setQuery(text)
+      setText("")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <section className="section">
+      <form autoComplete="off" onSubmit={handleSubmit}> 
+        <input 
+        type="text" 
+        name="search" 
+        id="search"
+        value={text} 
+        onChange = {(e) => setText(e.target.value)}
+        placeholder="Search For Something" />
+
+        <button id="search-btn">Search</button>
+        
+      </form>
+     {isLoading ? <div className="spinner"></div> : <>
+     <article className='title'>
+          <h1> {largeTitle.title}</h1>
+          <a href= {largeTitle.url} target="_blank" rel="noreferrer">Read Full Article </a>
+        </article>
+
+        <p className="category"> 
+        Category: <span>{query}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       
+        <article className="cards"> 
+         
+          {items.map(({author, created_at, title, url, objectId}) => (
+            <div key = {objectId}>
+              <h2>{title}</h2>
+              <ul>
+                <li> By {author}</li>
+                <li> <a href= {url} target="_blank" rel="noreferrer">Read Full Article </a></li>
+             
+              </ul>
+              <p>{format(new Date(created_at),"dd MMMM yyyy")}</p>
+              </div>
+          ))}
+        </article>
+     </> }
+      </section>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import HelloWorld from './HelloWorld';
+
+test('renders "Hello, World!"', () => {
+  render(<HelloWorld />);
+  const helloText = screen.getByText(/Hello, World!/i);
+  expect(helloText).toBeInTheDocument();
 });
